12e6fd2bfdcc25068858112040318d08e4fc07fb
sbylash <salsabyla3008@gmail.com>
2024-11-15 08:13:59 +0700

    update

--- Jobsheet10/android/app/src/main/kotlin/com/example/master_plan/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.master_plan
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Jobsheet10/lib/models/task.dart
+++
@@ -1,9 +0,0 @@
-class Task {
-  final String description;
-  final bool complete;
-  
-  const Task({
-    this.complete = false,
-    this.description = '',
-  });
-}
